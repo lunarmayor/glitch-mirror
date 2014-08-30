34eee0c1ed51b1698c9e43a705dfee2bddacdb45
Michael Morrissey <mmorrissey@market76.com>
2014-08-30 14:31:24 -0400

    project start:

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GlitchMirror::Application.config.secret_token = 'f90606128851fa33fc4d0374aeeedaa35a1b286c35f4713b88482ab1127128cdef6446e529e23f8f614013cb361224c78caa5a6275b36c4d87d5b683ef44fe03'
